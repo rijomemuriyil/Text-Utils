feae6b42bbb1a6d52e278886ecc9411cecef351f
rijomemuriyil <rijomemuriyil18@gmail.com>
2023-05-10 21:31:59 +0530

    Trigger rebuild

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 import Alert from './components/Alert';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React,{useState} from 'react';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 function App() {
 
@@ -79,20 +78,26 @@ function App() {
         </div>
    
  </Router> */}
-
+ <Navbar title="TextUtils" aboutText="About TextUtils"  mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>  
+      <div className="container my-3">
+      <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>
+      </div>
+      
+{/* 
  <Router>
       <Navbar title="TextUtils" aboutText="About TextUtils"  mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>  
       <div className="container my-3">
         <Routes>   
-          {/* /users  -->Component 1
-          /users/home -->Component2 */}
+          // /users  -->Component 1
+          // /users/home -->Component2
 
           <Route exact path="/about" element={<About/>} />
           <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>} />
         </Routes>
       </div>
-   </Router>
+   </Router> */}
 
 
      </>
